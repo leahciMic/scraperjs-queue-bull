bdcf484b250f282219daf67433eb267623f562ed
Michael Leaney <mleaney@tokbox.com>
2016-06-17 15:08:15 +1000

    Add REDIS_PORT and REDIS_HOST environment variable support

--- src/queue-bull.js
+++ src/queue-bull.js
@@ -1,9 +1,12 @@
 import createNormalQueue from 'bull';
 import createPriorityQueue from 'bull/lib/priority-queue';
 
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1';
+
 export default function createBullQueue(name) {
   const createQueue = process.env.PRIORITY_QUEUE ? createPriorityQueue : createNormalQueue;
-  const queue = createQueue(name);
+  const queue = createQueue(name, REDIS_PORT, REDIS_HOST);
   return {
     process(fn) {
       const callbackWrapper = (job, done) => fn(job.data, done);
