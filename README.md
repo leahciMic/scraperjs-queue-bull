810bfe444e0e5792c4d8d65d8b66917ee4b84f13
Michael Leaney <mleaney@tokbox.com>
2016-06-28 22:25:20 +1000

    Move url cache into queue
    Scraper.js is removing the idea of a urlcache, queues are now expected to
    manage this themselves.

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-# A queue for scraper.js backed by bull
+# A queue plugin for Scraper.js that is backed by Bull
 
-Define an environmental property called `PRIORITY_QUEUE` to use a priority queue.
+## Methods
+
+### process(function)
+
+The supplied function will be called for each item in the queue one at a time.
+
+### add(queueItem, opts)
+
+Add's a queueItem.
+
+### count()
+
+Returns a promise that will resolve to the remaining items in the queue.
+
+### close()
+
+Close everything.
+
+## Environment variables
+
+`PRIORITY_QUEUE`, if defined, a priority queue will be used
+`REDIS_PORT`, the port for Redis
+`REDIS_HOST`, the host for Redis
+
+## Todo
+
+* More documentation.
+* Tests.
+* Remove environment variables, and pass these things in from scraper.js.
--- src/queue-bull.js
+++ src/queue-bull.js
@@ -1,12 +1,27 @@
 import createNormalQueue from 'bull';
 import createPriorityQueue from 'bull/lib/priority-queue';
+import redis from 'redis';
+import bluebird from 'bluebird';
+
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
 
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
 const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1';
 
-export default function createBullQueue(name) {
+function createClient() {
+  return redis.createClient({
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+  });
+}
+
+const KEY_PREFIX = 'scraper.js-queue-bull-url-cache';
+
+export default function createBullQueue(name, redisClient = createClient) {
   const createQueue = process.env.PRIORITY_QUEUE ? createPriorityQueue : createNormalQueue;
   const queue = createQueue(name, REDIS_PORT, REDIS_HOST);
+
   return {
     process(fn) {
       const callbackWrapper = (job, done) => fn(job.data, done);
@@ -14,8 +29,31 @@ export default function createBullQueue(name) {
 
       queue.process(fn.length === 2 ? callbackWrapper : promiseWrapper);
     },
-    add(queueItem, opts) {
-      return queue.add(queueItem, opts);
+    async add(queueItem, {
+      expiry = 86400000,
+      priority = 'high',
+      attempts = 2,
+      backoff = 3600000,
+     } = {}) {
+      const key = `${KEY_PREFIX}:(${queueItem.url})`;
+      const saved = await redisClient.setnxAsync(key, 'true');
+
+      if (saved === 1) {
+        await queue.add(queueItem, {
+          priority,
+          attempts,
+          backoff,
+        });
+        await redisClient.setpxAsync(key, expiry);
+      }
+
+      return true;
+    },
+    count() {
+      return queue.count();
+    },
+    close() {
+      return queue.close();
     },
   };
 }
